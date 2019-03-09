c520b4919409f2e853c2b9d31a1f40e6da9fbef5
Luke Herrmann <herrmannluke@gmail.com>
2019-03-09 10:57:17 -0700

    Address Controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,50 @@
 class AddressesController < ApplicationController
+  before_action :set_location
+  before_action :set_address, only: [:show, :edit, :update, :destroy]
+
   def index
+    @addresses = Address.all
   end
 
   def show
   end
 
   def new
+    @address = @location.addresses.new
+  end
+
+  def create
+    @address = @location.addresses.new(address_params)
+    if @address.save 
+      redirect_to trip_locations_path[@location, @address]
+    else
+      render :new
+    end
   end
 
   def edit
+    if @address.update(address_params)
+      redirect_to trip_locations_path[@location, @address]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @address.destroy
+    redirect_to trip_locations_path
+  end
+
+  private
+  def set_location
+    @location = Location.find(params[:location_id])
+  end
+
+  def address_params
+    params.require(:address).permit(:street, :city, :state, :zip, :latitude, :longitude)
+  end
+
+  def set_address
+    @address = Address.find(params[:id])
   end
 end
